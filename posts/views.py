2d7127807468db872004bf3d40ec4554580ba413
Даниил <grygorev37@gmail.com>
2021-02-10 01:05:46 +0700

    SECOND VER

--- posts/views.py
+++ posts/views.py
@@ -11,7 +11,6 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
-
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
@@ -19,7 +18,6 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
-
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
