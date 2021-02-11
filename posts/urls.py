9eda3cbb4d5ed18f5779f3e76c993de79181566a
Даниил <grygorev37@gmail.com>
2021-02-12 03:13:12 +0700

    FOURTH VER

--- posts/urls.py
+++ posts/urls.py
@@ -1,15 +1,17 @@
 from django.urls import include, path
 from rest_framework.authtoken import views as auth_views
 from rest_framework.routers import DefaultRouter
+
 from yatube_api.settings import API_URL_START
+
 from .views import CommentViewSet, PostViewSet
 
 API_VERSION = 'v1'
 
 router = DefaultRouter()
-router.register(f'/posts', PostViewSet,
+router.register('/posts', PostViewSet,
                 basename='posts')
-router.register(fr'^/posts/(?P<post_id>.+)/comments',
+router.register(r'^/posts/(?P<post_id>.+)/comments',
                 CommentViewSet,
                 basename='posts')
 
