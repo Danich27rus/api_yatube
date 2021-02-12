8772cd99f6805d0e1dce8471b2404d5d01185325
Даниил <grygorev37@gmail.com>
2021-02-12 07:22:56 +0700

    Fifth VER

--- posts/urls.py
+++ posts/urls.py
@@ -2,21 +2,24 @@ from django.urls import include, path
 from rest_framework.authtoken import views as auth_views
 from rest_framework.routers import DefaultRouter
 
-from yatube_api.settings import API_URL_START
-
 from .views import CommentViewSet, PostViewSet
 
 API_VERSION = 'v1'
 
+file = open("api_prefix.txt", 'r')
+content = file.read()
+print(content)
+file.close()
+
 router = DefaultRouter()
-router.register('/posts', PostViewSet,
+router.register('posts', PostViewSet,
                 basename='posts')
-router.register(r'^/posts/(?P<post_id>.+)/comments',
+router.register(r'^posts/(?P<post_id>.+)/comments',
                 CommentViewSet,
                 basename='posts')
 
 urlpatterns = [
-    path(f'{API_URL_START}/{API_VERSION}/api-token-auth/',
+    path(f'{content}/{API_VERSION}/api-token-auth/',
          auth_views.obtain_auth_token),
-    path(f'{API_URL_START}/{API_VERSION}', include(router.urls))
+    path(f'{content}/{API_VERSION}/', include(router.urls))
 ]
